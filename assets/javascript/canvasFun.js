fb00352b65b9bdf00befd23d989ad9a5f968be48
unknown <mrhdigital@yahoo.com>
2019-11-30 11:52:34 -0600

    basic line draw works

--- assets/javascript/canvasFun.js
+++ assets/javascript/canvasFun.js
@@ -21,11 +21,18 @@ function draw(e) {
     // go to point
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
-    lastX = e.offsetX;
-    lastY = e.offsetY;
+    // lastX = e.offsetX;
+    // lastY = e.offsetY;
+    // above code can be written as follow:
+    [lastX, lastY] = [e.offsetX, e.offsetY];
 
 }
+canvas.addEventListener("mousedown",(e) => { 
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+
 canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown",() => isDrawing = true);
 canvas.addEventListener("mouseup",() => isDrawing = false);
 canvas.addEventListener("mouseout",() => isDrawing = false);
