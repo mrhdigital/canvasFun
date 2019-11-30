723e630277f29c9b022efd88e49a62f0404f38b7
unknown <mrhdigital@yahoo.com>
2019-11-30 11:32:11 -0600

    mousemove event only trigger when muse down console.log

--- assets/javascript/canvasFun.js
+++ assets/javascript/canvasFun.js
@@ -12,6 +12,11 @@ let lastX = 0;
 let lastY = 0;
 
 function draw(e) {
+    // stop the function running when mouse down in not used
+    if(!isDrawing) return;
     console.log(e);
 }
 canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown",() => isDrawing = true);
+canvas.addEventListener("mouseup",() => isDrawing = false);
+canvas.addEventListener("mouseout",() => isDrawing = false);
