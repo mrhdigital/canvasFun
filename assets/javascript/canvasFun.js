12c3d676f56ac2259412eba03c0b775890b52c66
unknown <mrhdigital@yahoo.com>
2019-11-30 11:04:05 -0600

    create canvas

--- assets/javascript/canvasFun.js
+++ assets/javascript/canvasFun.js
@@ -1,1 +1,6 @@
-alert("Linked ");
+//alert("Linked ");
+
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
