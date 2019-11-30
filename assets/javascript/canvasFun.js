33b980f01e0f1fa89697e0cde565a23a69706b78
unknown <mrhdigital@yahoo.com>
2019-11-30 11:17:18 -0600

    mousemove event works in console.log

--- assets/javascript/canvasFun.js
+++ assets/javascript/canvasFun.js
@@ -4,3 +4,14 @@ const canvas = document.querySelector("#draw");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = "BASA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+    console.log(e);
+}
+canvas.addEventListener("mousemove", draw);
